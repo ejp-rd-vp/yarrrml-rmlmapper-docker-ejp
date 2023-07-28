9abdffb7572abf5c05c8eaa9c1eeb33f6f4549b6
Mark Wilkinson <markw@illuminae.com>
2023-07-28 20:00:16 +0200

    ready for FiaBv2

--- docker/t.rb
+++ docker/t.rb
@@ -4,32 +4,34 @@ require 'sinatra'
 require 'open3'
 
 get '/:type' do
+  begin
     `mkdir /mnt/data/triples`
-    type = params[:type]
-    serialization = ENV['SERIALIZATION'] || "nquads"
-    # (nquads (default), trig, trix, jsonld, hdt, turtle)
-    extension = "rdf"
-    case serialization
-    when "trig"
-        extension = "trig"
-    when "trix" 
-        extension = "trix"
-    when "jsonld"
-        extension = "json"
-    when "hdt"
-        extension = "hdt"
-    when "nquads"
-        extension = "nq"
-    when "turtle"
-        extension = "ttl"
-    end
+  rescue StandardError
+    warn "triples folder coiuldn't be created.  Might already exist"
+  end
+  type = params[:type]
+  serialization = ENV['SERIALIZATION'] || 'nquads'
+  # (nquads (default), trig, trix, jsonld, hdt, turtle)
+  extension = 'rdf'
+  case serialization
+  when 'trig'
+    extension = 'trig'
+  when 'trix'
+    extension = 'trix'
+  when 'jsonld'
+    extension = 'json'
+  when 'hdt'
+    extension = 'hdt'
+  when 'nquads'
+    extension = 'nq'
+  when 'turtle'
+    extension = 'ttl'
+  end
 
-    puts "#{type}"
-    #rml = "#{type}_rml.ttl"
-    yarrrml = "/mnt/data/#{type}_yarrrml.yaml"
-    #datafile = "#{type}.#{extension}"
-    a, b, c = Open3.capture3("bash map.sh #{yarrrml} --outputfile /mnt/data/triples/#{type}.#{extension} --serialization #{serialization}")
-    puts a,b,c
-
-
+  puts type.to_s
+  # rml = "#{type}_rml.ttl"
+  yarrrml = "/mnt/data/#{type}_yarrrml.yaml"
+  # datafile = "#{type}.#{extension}"
+  a, b, c = Open3.capture3("bash map.sh #{yarrrml} --outputfile /mnt/data/triples/#{type}.#{extension} --serialization #{serialization}")
+  puts a, b, c
 end
